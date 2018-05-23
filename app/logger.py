93e6df31afafaa84fe4f1e0ef4a12022098e8527
Anne Schumacher <anne.schumacher@gu.se>
2018-05-23 08:31:09 +0000

    fix in logging module

--- app/logger.py
+++ app/logger.py
@@ -8,17 +8,11 @@ from config import Config
 # Set debug level: DEBUG, INFO, WARNING, ERROR, CRITICAL
 DEBUGLEVEL = logging.INFO
 
-# Write to stdout or logfile?
-DEBUG_TO_STDOUT = True if Config.log_dir is None else False
-
 # Format for log messages and date
 LOGFMT = '%(asctime)-15s - %(levelname)s: %(message)s'
 DATEFMT = '%Y-%m-%d %H:%M:%S'
 
-if DEBUG_TO_STDOUT:
-    logging.basicConfig(stream=sys.stdout, level=DEBUGLEVEL,
-                        format=LOGFMT, datefmt=DATEFMT)
-else:
+if getattr(Config, "log_dir", False):
     today = time.strftime("%Y%m%d")
     DEBUGFILE = os.path.join(Config.log_dir, '%s-backend.log' % today)
     # Create Logfile if it does not exist
@@ -26,8 +20,11 @@ else:
         with open(DEBUGFILE, "w") as f:
             now = time.strftime("%Y-%m-%d %H:%M:%S")
             f.write("%s CREATED DEBUG FILE\n\n" % now)
-        # Fix permissions
-        os.chmod(DEBUGFILE, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH |
-                 stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH)
-    logging.basicConfig(filename=DEBUGFILE, level=DEBUGLEVEL,
+            # Fix permissions
+            os.chmod(DEBUGFILE, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH |
+            stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH)
+            logging.basicConfig(filename=DEBUGFILE, level=DEBUGLEVEL,
+            format=LOGFMT, datefmt=DATEFMT)
+else:
+    logging.basicConfig(stream=sys.stdout, level=DEBUGLEVEL,
                         format=LOGFMT, datefmt=DATEFMT)
