b2d6d14c939741cd8da02aead3888aa1f3180601
Anne Schumacher <anne.schumacher.up@gmail.com>
2018-10-16 12:02:05 +0200

    reordered routes

--- app/handlers.py
+++ app/handlers.py
@@ -2,7 +2,7 @@
 
 from future import standard_library
 standard_library.install_aliases()
-from flask import Flask, Response, request, json, render_template, send_from_directory, jsonify
+from flask import Flask, Response, request, render_template, send_from_directory, jsonify
 from flask_cors import CORS
 from xml.sax.saxutils import escape
 import logging
@@ -39,6 +39,94 @@ def hello_world():
     """For debugging purposes."""
     log.info("Hello!")
     return jsonify({'message': 'Hello!'}), 200
+
+
+@app.route('/', methods=['GET', 'POST'])
+def text_input():
+    # On empty input return api description
+    if not request.values:
+        return api()
+
+    try:
+        log.info("Starting a new build with text input procedure")
+        lang = request.values.get('language', 'sv')
+        mode = request.values.get('mode', 'plain')
+        txt = request.values.get('text', '')
+
+        builds = app.config["BUILDS"]
+        settings, incremental = get_settings(lang, mode)
+
+        def generate(mode, builds, txt, settings, incremental):
+            yield "<result>\n".encode("UTF-8")
+            # Escape plain text and give it a root element
+            if mode == "plain":
+                txt = escape(txt)
+                txt = "<text>" + txt + "</text>"
+            # Check for empty input
+            if not txt:
+                log.exception(ERROR_MSG["empty_input"])
+                yield "<error>%s</error>\n</result>" % ERROR_MSG["empty_input"]
+            else:
+                for node in build(builds, txt, settings, incremental, "xml"):
+                    yield node
+
+        return Response(generate(mode, builds, txt, settings, incremental), mimetype='application/xml')
+    except:
+        trace = make_trace()
+        log.exception("Error in text input procedure")
+        res = '<result>\n<trace>' + escape(trace) + '</trace>\n</result>\n'
+        return Response(res, mimetype='application/xml')
+
+
+@app.route('/schema')
+def schema():
+    """The /schema handler"""
+    lang = request.values.get('language', 'sv')
+    mode = request.values.get('mode', 'plain')
+    minify = request.values.get('minify', 'False')
+    minify = minify.lower() == 'true'
+    return_json = make_schema(lang, mode, minify)
+    # print(json.dumps(return_json, indent=4, separators=(',', ': '), ensure_ascii=False))
+    return jsonify(return_json), 200
+
+
+@app.route('/makefile', methods=['GET', 'POST'])
+def get_makefile():
+    """Handler for returning the makefile."""
+    try:
+        lang = request.values.get('language', 'sv')
+        mode = request.values.get('mode', 'plain')
+        settings, incremental = get_settings(lang, mode)
+        log.info("Returning makefile")
+        return Response(makefile(settings), mimetype='text/plain')
+    except:
+        trace = make_trace()
+        log.exception("Error in /makefile")
+        res = '<result>\n<trace>' + escape(trace) + '</trace>\n</result>\n'
+        return Response(res, mimetype='application/xml')
+
+
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+    """Handler for joining an existing build."""
+    try:
+        log.info("Joining existing build")
+        builds = app.config["BUILDS"]
+        hashnumber = request.values.get('hash', '')
+        incremental = request.values.get('incremental', '')
+        incremental = incremental.lower() == 'true'
+
+        def generate(builds, hashnumber, incremental):
+            yield "<result>\n"
+            for node in join_from_hash(builds, hashnumber, incremental):
+                yield node
+
+        return Response(generate(builds, hashnumber, incremental), mimetype='text/plain')
+    except:
+        trace = make_trace()
+        log.exception("Error in /join")
+        res = '<result>\n<trace>' + escape(trace) + '</trace>\n</result>\n'
+        return Response(res, mimetype='application/xml')
 
 
 @app.route('/api')
@@ -171,204 +259,6 @@ def ping():
         return jsonify(data), status
 
 
-@app.route('/schema')
-def schema():
-    """The /schema handler"""
-    lang = request.values.get('language', 'sv')
-    mode = request.values.get('mode', 'plain')
-    minify = request.values.get('minify', 'False')
-    minify = minify.lower() == 'true'
-    return_json = make_schema(lang, mode, minify)
-    # print(json.dumps(return_json, indent=4, separators=(',', ': '), ensure_ascii=False))
-    return jsonify(return_json), 200
-
-
-@app.route('/cleanup')
-def cleanup(timeout=604800, remove_errors=False):
-    """
-    The /cleanup handler.
-    Remove builds that are finished and haven't been accessed within the timeout,
-    which is by default 7 days.
-    With remove_errors, removes the builds with status Error.
-    Requires secret_key parameter in query.
-    """
-    secret_key = request.values.get('secret_key', '')
-    if check_secret_key(secret_key):
-        builds = app.config["BUILDS"]
-        to_remove = []
-        for h, b in builds.items():
-            # log.info("accessed_time %s, hash %s" % (time.time() - b.accessed_time, h))
-            if (finished(b.status) and time.time() - b.accessed_time > timeout or
-                    b.status in [Status.Error, Status.ParseError] and remove_errors):
-                to_remove.append((h, b))
-        data = {"removed_builds": []}
-        for h, b in to_remove:
-            log.info("Removing %s" % h)
-            b.remove_files()
-            del builds[h]
-            data["removed_builds"].append("%s" % h)
-        if len(data["removed_builds"]) == 0:
-            data = {"message": "No hashes to be removed."}
-        status = 200
-    else:
-        msg = "Failed to run cleanup: secret key could not be confirmed."
-        data = jsonify_error(msg)
-        status = 401
-
-    return jsonify(data), status
-
-
-@app.route('/cleanup/errors')
-def cleanup_errors():
-    return cleanup(remove_errors=True)
-
-
-@app.route('/cleanup/forceall')
-def cleanup_all():
-    """Remove all the existing builds. Requires secret_key parameter in query."""
-    builds = app.config["BUILDS"]
-    to_remove = []
-    secret_key = request.values.get('secret_key', '')
-
-    if check_secret_key(secret_key):
-        log.info("All builds will be removed.")
-        build_dirs = get_build_directories(Config.builds_dir)
-        for hashnumber in build_dirs:
-            b = builds.get(hashnumber, None)
-            log.info("Removing %s" % hashnumber)
-            if b:
-                to_remove.append(hashnumber)
-                b.remove_files()
-            else:
-                rmdir(os.path.join(Config.builds_dir, hashnumber))
-        data = {"removed_builds": []}
-        for h in to_remove:
-            del builds[h]
-            data["removed_builds"].append("%s" % h)
-        if len(data["removed_builds"]) == 0:
-            data = {"message": "No hashes to be removed."}
-        status = 200
-    else:
-        log.error("No builds will be removed.")
-        msg = "Failed to run cleanup: secret key could not be confirmed."
-        data = jsonify_error(msg)
-        status = 401
-
-    return jsonify(data), status
-
-
-@app.route('/cleanup/forceone')
-def cleanup_one():
-    """Remove a single (problematic) build."""
-    builds = app.config["BUILDS"]
-    secret_key = request.values.get('secret_key', '')
-    hash = request.values.get('hash', '')
-
-    if not hash:
-        msg = "Don't know what to remove. Please enter hash number in query!"
-        data = jsonify_error(msg)
-        return jsonify(data), 400
-
-    if check_secret_key(secret_key):
-        b = builds.get(hash, None)
-        if b:
-            log.info("Removing %s" % hash)
-            del builds[hash]
-            b.remove_files()
-            data = {"removed_builds": ["%s" % hash]}
-        else:
-            msg = "Failed to remove build: hash not found, trying to remove files."
-            log.error(msg)
-            data = jsonify_error(msg)
-            status = 400
-            if hash in get_build_directories(Config.builds_dir):
-                rmdir(os.path.join(Config.builds_dir, hash))
-                log.info("Files removed for hash %s" % hash)
-            else:
-                log.info("No files to be removed for hash %s" % hash)
-    else:
-        msg = "Failed to remove all builds: secret key could not be confirmed."
-        log.error(msg)
-        data = jsonify_error(msg)
-        status = 401
-
-    return jsonify(data), status
-
-
-@app.route('/makefile', methods=['GET', 'POST'])
-def get_makefile():
-    """Handler for returning the makefile."""
-    try:
-        lang = request.values.get('language', 'sv')
-        mode = request.values.get('mode', 'plain')
-        settings, incremental = get_settings(lang, mode)
-        log.info("Returning makefile")
-        return Response(makefile(settings), mimetype='text/plain')
-    except:
-        trace = make_trace()
-        log.exception("Error in /makefile")
-        res = '<result>\n<trace>' + escape(trace) + '</trace>\n</result>\n'
-        return Response(res, mimetype='application/xml')
-
-
-@app.route('/', methods=['GET', 'POST'])
-def text_input():
-    # On empty input return api description
-    if not request.values:
-        return api()
-
-    try:
-        log.info("Starting a new build with text input procedure")
-        lang = request.values.get('language', 'sv')
-        mode = request.values.get('mode', 'plain')
-        txt = request.values.get('text', '')
-
-        builds = app.config["BUILDS"]
-        settings, incremental = get_settings(lang, mode)
-
-        def generate(mode, builds, txt, settings, incremental):
-            yield "<result>\n".encode("UTF-8")
-            # Escape plain text and give it a root element
-            if mode == "plain":
-                txt = escape(txt)
-                txt = "<text>" + txt + "</text>"
-            # Check for empty input
-            if not txt:
-                log.exception(ERROR_MSG["empty_input"])
-                yield "<error>%s</error>\n</result>" % ERROR_MSG["empty_input"]
-            else:
-                for node in build(builds, txt, settings, incremental, "xml"):
-                    yield node
-
-        return Response(generate(mode, builds, txt, settings, incremental), mimetype='application/xml')
-    except:
-        trace = make_trace()
-        log.exception("Error in text input procedure")
-        res = '<result>\n<trace>' + escape(trace) + '</trace>\n</result>\n'
-        return Response(res, mimetype='application/xml')
-
-
-@app.route('/join', methods=['GET', 'POST'])
-def join():
-    """Handler for joining an existing build."""
-    try:
-        log.info("Joining existing build")
-        builds = app.config["BUILDS"]
-        hashnumber = request.values.get('hash', '')
-        incremental = request.values.get('incremental', '')
-        incremental = incremental.lower() == 'true'
-
-        def generate(builds, hashnumber, incremental):
-            yield "<result>\n"
-            for node in join_from_hash(builds, hashnumber, incremental):
-                yield node
-
-        return Response(generate(builds, hashnumber, incremental), mimetype='text/plain')
-    except:
-        trace = make_trace()
-        log.exception("Error in /join")
-        res = '<result>\n<trace>' + escape(trace) + '</trace>\n</result>\n'
-        return Response(res, mimetype='application/xml')
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -421,6 +311,118 @@ def download():
 
     return send_from_directory(filepath, filename, mimetype=mimetype,
                                as_attachment=True, attachment_filename=attachment_filename)
+
+
+@app.route('/cleanup')
+def cleanup(timeout=604800, remove_errors=False):
+    """
+    The /cleanup handler.
+    Remove builds that are finished and haven't been accessed within the timeout,
+    which is by default 7 days.
+    With remove_errors, removes the builds with status Error.
+    Requires secret_key parameter in query.
+    """
+    secret_key = request.values.get('secret_key', '')
+    if check_secret_key(secret_key):
+        builds = app.config["BUILDS"]
+        to_remove = []
+        for h, b in builds.items():
+            # log.info("accessed_time %s, hash %s" % (time.time() - b.accessed_time, h))
+            if (finished(b.status) and time.time() - b.accessed_time > timeout or
+                    b.status in [Status.Error, Status.ParseError] and remove_errors):
+                to_remove.append((h, b))
+        data = {"removed_builds": []}
+        for h, b in to_remove:
+            log.info("Removing %s" % h)
+            b.remove_files()
+            del builds[h]
+            data["removed_builds"].append("%s" % h)
+        if len(data["removed_builds"]) == 0:
+            data = {"message": "No hashes to be removed."}
+        status = 200
+    else:
+        msg = "Failed to run cleanup: secret key could not be confirmed."
+        data = jsonify_error(msg)
+        status = 401
+
+    return jsonify(data), status
+
+
+@app.route('/cleanup/errors')
+def cleanup_errors():
+    return cleanup(remove_errors=True)
+
+
+@app.route('/cleanup/forceone')
+def cleanup_one():
+    """Remove a single (problematic) build."""
+    builds = app.config["BUILDS"]
+    secret_key = request.values.get('secret_key', '')
+    hash = request.values.get('hash', '')
+
+    if not hash:
+        msg = "Don't know what to remove. Please enter hash number in query!"
+        data = jsonify_error(msg)
+        return jsonify(data), 400
+
+    if check_secret_key(secret_key):
+        b = builds.get(hash, None)
+        if b:
+            log.info("Removing %s" % hash)
+            del builds[hash]
+            b.remove_files()
+            data = {"removed_builds": ["%s" % hash]}
+        else:
+            msg = "Failed to remove build: hash not found, trying to remove files."
+            log.error(msg)
+            data = jsonify_error(msg)
+            status = 400
+            if hash in get_build_directories(Config.builds_dir):
+                rmdir(os.path.join(Config.builds_dir, hash))
+                log.info("Files removed for hash %s" % hash)
+            else:
+                log.info("No files to be removed for hash %s" % hash)
+    else:
+        msg = "Failed to remove all builds: secret key could not be confirmed."
+        log.error(msg)
+        data = jsonify_error(msg)
+        status = 401
+
+    return jsonify(data), status
+
+
+@app.route('/cleanup/forceall')
+def cleanup_all():
+    """Remove all the existing builds. Requires secret_key parameter in query."""
+    builds = app.config["BUILDS"]
+    to_remove = []
+    secret_key = request.values.get('secret_key', '')
+
+    if check_secret_key(secret_key):
+        log.info("All builds will be removed.")
+        build_dirs = get_build_directories(Config.builds_dir)
+        for hashnumber in build_dirs:
+            b = builds.get(hashnumber, None)
+            log.info("Removing %s" % hashnumber)
+            if b:
+                to_remove.append(hashnumber)
+                b.remove_files()
+            else:
+                rmdir(os.path.join(Config.builds_dir, hashnumber))
+        data = {"removed_builds": []}
+        for h in to_remove:
+            del builds[h]
+            data["removed_builds"].append("%s" % h)
+        if len(data["removed_builds"]) == 0:
+            data = {"message": "No hashes to be removed."}
+        status = 200
+    else:
+        log.error("No builds will be removed.")
+        msg = "Failed to run cleanup: secret key could not be confirmed."
+        data = jsonify_error(msg)
+        status = 401
+
+    return jsonify(data), status
 
 
 @app.route('/easteregg')
